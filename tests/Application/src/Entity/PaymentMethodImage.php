df58b57718d38f7555b185d7ba2815d9601ed199
jakub-czech <jakub.czech@commerceweavers.com>
2025-02-18 11:55:42 +0100

    code adjustment

--- tests/Application/src/Entity/PaymentMethodImage.php
+++ tests/Application/src/Entity/PaymentMethodImage.php
@@ -6,7 +6,6 @@ namespace App\Entity;
 
 use Sylius\Component\Core\Model\Image;
 use Doctrine\ORM\Mapping as ORM;
-use Sylius\Resource\Metadata\AsResource;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'sylius_payment_method_image')]
