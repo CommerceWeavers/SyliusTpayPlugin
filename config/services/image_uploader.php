2ec15f7075347a70c16f2ab764ddc58ce4e4aa72
jakubczech <jakub.czech@commerceweavers.com>
2025-02-19 16:08:06 +0100

    CR adjustments - remove comment

--- config/services/image_uploader.php
+++ config/services/image_uploader.php
@@ -9,8 +9,6 @@ use Sylius\Bundle\CoreBundle\EventListener\ImageUploadListener;
 return static function(ContainerConfigurator $container): void {
     $services = $container->services();
 
-    /** new */
-
     $services->set('commerce_weavers_sylius_tpay.listener.payment_method.image_upload', ImageUploadListener::class)
         ->args([service('sylius.image_uploader')])
         ->tag('kernel.event_listener', ['event' => 'sylius.payment_method.pre_create', 'method' => 'uploadImage'])
