1fafbd69b1712faa1e6b64d68a17909b04212998
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2025-08-06 07:45:18 +0200

    Configure Refund plugin assets

--- tests/Application/webpack.config.js
+++ tests/Application/webpack.config.js
@@ -7,4 +7,22 @@ const SyliusShop = require('@sylius-ui/shop');
 const adminConfig = SyliusAdmin.getWebpackConfig(path.resolve(__dirname));
 const shopConfig = SyliusShop.getWebpackConfig(path.resolve(__dirname));
 
-module.exports = [shopConfig, adminConfig];
+Encore.reset();
+
+// App admin config
+Encore
+  .setOutputPath('public/build/app/admin')
+  .setPublicPath('/build/app/admin')
+  .addEntry('app-admin-entry', './assets/admin/entrypoint.js')
+  .disableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .enableSassLoader();
+
+const appAdminConfig = Encore.getWebpackConfig();
+
+appAdminConfig.externals = Object.assign({}, appAdminConfig.externals, { window: 'window', document: 'document' });
+appAdminConfig.name = 'app.admin';
+
+module.exports = [appAdminConfig, shopConfig, adminConfig];
