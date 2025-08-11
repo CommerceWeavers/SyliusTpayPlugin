bcd98b99b08edd37b1668218cf7de5adfd373794
Jacob Tobiasz <jakub.tobiasz@icloud.com>
2025-08-11 07:16:38 +0200

    Bring back functionality to remove the payment method image with a confirmation message

--- src/Controller/Admin/RemovePaymentMethodImageAction.php
+++ src/Controller/Admin/RemovePaymentMethodImageAction.php
@@ -9,17 +9,18 @@ use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\FlashBagAwareSessionInterface;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 
-final class RemovePaymentMethodImageAction
+final readonly class RemovePaymentMethodImageAction
 {
     public function __construct(
-        private readonly RepositoryInterface $paymentMethodImageRepository,
-        private readonly RouterInterface $router,
-        private readonly CsrfTokenManagerInterface $csrfTokenManager,
+        private RepositoryInterface $paymentMethodImageRepository,
+        private RouterInterface $router,
+        private CsrfTokenManagerInterface $csrfTokenManager,
     ) {
     }
 
@@ -39,6 +40,14 @@ final class RemovePaymentMethodImageAction
             $this->paymentMethodImageRepository->remove($paymentMethodImage);
         }
 
+        $session = $request->getSession();
+        $this->addFlashMessage($session, 'success', 'commerce_weavers_sylius_tpay.admin.payment_method.image_has_been_removed');
+
         return new RedirectResponse($this->router->generate('sylius_admin_payment_method_update', ['id' => $paymentMethodId]));
+    }
+
+    private function addFlashMessage(FlashBagAwareSessionInterface $session, string $type, string $message): void
+    {
+        $session->getFlashBag()->add($type, $message);
     }
 }
