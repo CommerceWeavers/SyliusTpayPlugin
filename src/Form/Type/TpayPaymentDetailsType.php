c5d5c592858da4d08535d8aa7e8571522611fa63
Paweł Czyżewski <pawel.czyzewski@commerceweavers.com>
2025-09-10 16:12:59 +0200

    added RemoveUnnecessaryPaymentDetailsFieldsListener.php

--- src/Form/Type/TpayPaymentDetailsType.php
+++ src/Form/Type/TpayPaymentDetailsType.php
@@ -15,17 +15,17 @@ use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Validator\Constraints\Length;
-use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Regex;
 
 final class TpayPaymentDetailsType extends AbstractType
 {
     public function __construct(
+        private readonly object $removeUnnecessaryPaymentDetailsFieldsListener,
         private readonly TokenStorageInterface $tokenStorage,
-    )
-    {
+    ) {
     }
 
     /**
@@ -48,9 +48,9 @@ final class TpayPaymentDetailsType extends AbstractType
                     'data' => null,
                     'property_path' => '[blik_token]',
                     'label' => 'commerce_weavers_sylius_tpay.shop.order_summary.blik.token',
+                    'validation_groups' => ['sylius_checkout_complete'],
                     'constraints' => [
-                        new NotBlank(groups: ['tpay_blik']),
-                        new Length(exactly: 6, groups: ['tpay_blik']),
+                        new Length(exactly: 6, groups: ['sylius_checkout_complete']),
                     ],
                 ],
             )
@@ -60,9 +60,9 @@ final class TpayPaymentDetailsType extends AbstractType
                 [
                     'property_path' => '[google_pay_token]',
                     'label' => false,
+                    'validation_groups' => ['sylius_checkout_complete'],
                     'constraints' => [
-                        new NotBlank(groups: ['tpay_google_pay']),
-                        new EncodedGooglePayToken(groups: ['tpay_google_pay']),
+                        new EncodedGooglePayToken(groups: ['sylius_checkout_complete']),
                     ],
                 ],
             )
@@ -72,9 +72,7 @@ final class TpayPaymentDetailsType extends AbstractType
                 [
                     'property_path' => '[apple_pay_token]',
                     'label' => false,
-                    'constraints' => [
-                        new NotBlank(groups: ['tpay_apple_pay']),
-                    ],
+                    'validation_groups' => ['sylius_checkout_complete'],
                 ],
             )
             ->add(
@@ -82,11 +80,11 @@ final class TpayPaymentDetailsType extends AbstractType
                 HiddenType::class,
                 [
                     'constraints' => [
-                        new NotBlank(groups: ['tpay_pbl']),
-                        new ValidTpayChannel(groups: ['tpay_pbl']),
+                        new ValidTpayChannel(groups: ['sylius_checkout_complete']),
                     ],
                     'error_bubbling' => false,
                     'property_path' => '[tpay_channel_id]',
+                    'validation_groups' => ['sylius_checkout_complete'],
                 ],
             )
             ->add(
@@ -98,18 +96,23 @@ final class TpayPaymentDetailsType extends AbstractType
                         'placeholder' => 'commerce_weavers_sylius_tpay.shop.order_summary.visa_mobile.placeholder',
                         'maxLength' => 15,
                     ],
+                    'validation_groups' => ['sylius_checkout_complete'],
                     'constraints' => [
-                        new NotBlank(groups: ['tpay_visa_mobile']),
-                        new Length(min: 7, max: 15, groups: ['tpay_visa_mobile']),
+                        new Length(min: 7, max: 15, groups: ['sylius_checkout_complete']),
                         new Regex(
                             '/^\d+$/',
                             message: 'commerce_weavers_sylius_tpay.shop.pay.visa_mobile.regex',
-                            groups: ['tpay_visa_mobile'],
+                            groups: ['sylius_checkout_complete'],
                         ),
                     ],
                     'label' => 'sylius.ui.phone_number',
                 ],
             );
+
+        $builder->addEventListener(
+            FormEvents::PRE_SUBMIT,
+            [$this->removeUnnecessaryPaymentDetailsFieldsListener, '__invoke'],
+        );
 
         $token = $this->tokenStorage->getToken();
         $user = $token?->getUser();
@@ -122,13 +125,15 @@ final class TpayPaymentDetailsType extends AbstractType
                     [
                         'label' => 'commerce_weavers_sylius_tpay.shop.order_summary.card.save_credit_card_for_later.label',
                     ],
-                );
+                )
+            ;
 
             $builder
                 ->add(
                     'use_saved_credit_card',
                     TpayCreditCardChoiceType::class,
-                );
+                )
+            ;
         }
     }
 }
